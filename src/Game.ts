064a1da36d49b0b4df49e07149764aba60d5cf5d
Igor <igortkachev94@gmail.com>
2026-01-30 21:10:50 +0700

    Update Game.ts

--- src/Game.ts
+++ src/Game.ts
@@ -1,50 +1,51 @@
 import { State } from "./State"
+import { Cell } from "./Board" 
 
-// Класс служит для определения типа объекта game из предыдущей работы с дополнительными методами.
 export class Game {
-    // Состояния игры
     steps: State[]
-    // Текущее состояние игры
     current: number
 
     constructor(
         steps: State[] = [new State()],
         current: number = 0
     ) {
-        // TODO
-        // Корректно инициализируйте переменные:
-        this.steps = []
-        this.current = 0
+        this.steps = steps.map(state => state.clone());
+        this.current = Math.min(current, this.steps.length - 1);
     }
 
     get state(): State {
-        // TODO
-        // Сеттер должен возвращать текущее состояние игры
-        return new State()
+        return this.steps[this.current].clone();
     }
 
-    clone() {
-        // TODO
-        // Функция должна вернуть копию объекта
-        return this        
+    clone(): Game {
+        return new Game(this.steps, this.current);
     }
 
     move(index: number): boolean {
-        // TODO        
-        // Определяет, какой символ ходит, и пытается сделать ход 
-        //  с помощью board.move.
-        // Если ход можно сделать, то добавляет  новыу позицию в steps, 
-        //  обновляет current и возвращает true, иначе возвращает false
-        // Нужно учесть, что если вызывалась функция toStep, то 
-        //  current можно указывать не на последний элемент steps
-        return true  
+        const currentState = this.steps[this.current];
+        const currentBoard = currentState.board.clone();
+        
+        if (!currentBoard.move(index, currentState.sym)) {
+            return false;
+        }
+
+        const nextSym: Cell = currentState.sym === "X" ? "0" : "X";  // Теперь Cell известен
+        const nextState = new State(currentBoard, nextSym);
+        
+        if (this.current < this.steps.length - 1) {
+            this.steps = this.steps.slice(0, this.current + 1);
+        }
+        
+        this.steps.push(nextState);
+        this.current = this.steps.length - 1;
+        return true;
     }
 
-    toStep(step: number) {
-         // TODO
-        // Проверяет, что в steps есть элемент с индексом step,
-        //  если нет то возвращает false
-        // Делает current равным step и обновляет свойство cell в board
-        return true       
+    toStep(step: number): boolean {
+        if (step < 0 || step >= this.steps.length) {
+            return false;
+        }
+        this.current = step;
+        return true;
     }
 }
