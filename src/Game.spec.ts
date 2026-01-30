6d9f5132ca0cdd092a4f6e2cbf194cd8501b9615
Igor <igortkachev94@gmail.com>
2026-01-30 21:09:17 +0700

    Update Game.spec.ts

--- src/Game.spec.ts
+++ src/Game.spec.ts
@@ -1,27 +1,34 @@
-import {Game} from './Game'
+import { Board } from "./Board";
 
-test("move", () => {
-    const game = new Game()
-    expect(game.move(0)).toBe(true)
-    expect(game.current).toBe(1)
-    expect(game.steps[1].board.cells[0]).toBe("X")
-    expect(game.move(0)).toBe(false)
-    expect(game.move(1)).toBe(true)
-    expect(game.current).toBe(2)
-    expect(game.steps[2].board.cells[0]).toBe("X")
-    expect(game.steps[2].board.cells[1]).toBe("0")
-    expect(game.steps[2].board.cells[3]).toBe("_")
+test("Конструктор", () => {
+    expect(new Board("00__XX___").cells.join("")).toBe("00__XX___");
+    expect(new Board("________").cells.join("")).toBe("_________");
+    expect(new Board("________1").cells.join("")).toBe("_________");
 });
 
-test("toStep", () => {
-    const state = new Game()
-    expect(state.toStep(0)).toBe(true)
-    expect(state.move(2)).toBe(true)
-    expect(state.move(1)).toBe(true)
-    expect(state.move(0)).toBe(true)
-    expect(state.toStep(4)).toBe(false)
-    expect(state.toStep(1)).toBe(true)
-    expect(state.steps.length).toBe(4)
-    expect(state.move(4)).toBe(true)
-    expect(state.steps.length).toBe(3)
-})
+test("isFill", () => {
+    expect(new Board("000XXX00_").isFill()).toBe(false);
+    expect(new Board("000XXX000").isFill()).toBe(true);
+});
+
+test("move", () => {
+    const board = new Board("000XXX00_");
+    expect(board.move(0, "0")).toBe(false);
+    expect(board.move(8, "X")).toBe(true);
+    expect(board.cells[8]).toBe("X");
+});
+
+test("checkWin", () => {
+    expect(new Board("000XXX00_").checkWin()).toBe("0");
+    expect(new Board("__X_X_X___").checkWin()).toBe("X");
+    expect(new Board("_________").checkWin()).toBe("_");
+});
+
+test("clone", () => {
+    const board1 = new Board("000XXX00_");
+    const board2 = board1.clone();
+    expect(board2.cells.join("")).toBe("000XXX00_");
+    board2.move(8, "X");
+    expect(board1.cells[8]).toBe("_"); // Проверяем глубокое копирование
+    expect(board2.cells[8]).toBe("X");
+});
