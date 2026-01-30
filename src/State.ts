66f3b4fb8617815323744af3425f1c28121e25d9
Igor <igortkachev94@gmail.com>
2026-01-30 21:14:13 +0700

    Update State.ts

--- src/State.ts
+++ src/State.ts
@@ -1,6 +1,5 @@
 import { Board, Cell } from "./Board"
 
-// Объекты класса хранят данные о состоянии игры: положение на доске и символ, который будет выставлен следующим ходом.
 export class State {
     board: Board
     sym: Cell
@@ -9,14 +8,11 @@ export class State {
         board: Board = new Board(),
         sym: Cell = "X"
     ) {
-        this.board = board
-        this.sym = sym
+        this.board = board.clone();
+        this.sym = sym;
     }
 
     clone(): State {
-        // TODO
-        // Функция должна вернуть копию объекта
-        return this
+        return new State(this.board.clone(), this.sym);
     }
-
 }
