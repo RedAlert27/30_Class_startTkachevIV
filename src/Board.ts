1b675b73d15890a5b42f3efc26a65d1f9e409faa
Igor <igortkachev94@gmail.com>
2026-01-30 21:08:54 +0700

    Update Board.ts

--- src/Board.ts
+++ src/Board.ts
@@ -1,48 +1,46 @@
 export type Cell = "_" | "X" | "0"
 
 export function isCell(sym: string): sym is Cell {
-    // TODO
-    // Проверяет, является ли sym типа Cell
-    // Возвращает true если sym типа Cell, иначе false
-    return true
+    return sym === "_" || sym === "X" || sym === "0";
 }
 
-// Класс служит для определения типа объекта board из предыдущей работы с дополнительными методами.
 export class Board {
     cells: Cell[]
 
     constructor(str: string | Cell[] = "_________") {
-        // TODO
-        // Используя сужения типа заполните this.cells
-        this.cells = []
+        if (Array.isArray(str)) {
+            this.cells = [...str];
+        } else {
+            const parsed = Board.fromString(str);
+            this.cells = parsed || ["_", "_", "_", "_", "_", "_", "_", "_", "_"];
+        }
     }
 
     clone(): Board {
-        // TODO
-        // Функция должна вернуть копию объекта
-        return this
+        return new Board([...this.cells]);
     }
 
     private static fromString(str: string): Cell[] | null {
-        // TODO
-        // Переписывает из str символы в this.cells
-        // Если длина строки не равна 9, возвращает null
-        // Если встретиться символ не из Cell возвращает null
-        // Если преобразование прошло успешно возвращает true
-        return null        
+        if (str.length !== 9) return null;
+        
+        const cells: Cell[] = [];
+        for (const char of str) {
+            if (!isCell(char)) return null;
+            cells.push(char);
+        }
+        return cells;
     }
 
     isFill(): boolean {
-        // TODO
-        // Возвращет true если на доске нет пустых клеток
-        return true
+        return !this.cells.includes("_");
     }
 
     move(index: number, cell: Cell): boolean {
-        // TODO
-        // Если ячейка this.cell[index] занята - возвращает false
-        // Записывает в ячейку cell и возвращает true
-        return true
+        if (index < 0 || index >= 9 || this.cells[index] !== "_") {
+            return false;
+        }
+        this.cells[index] = cell;
+        return true;
     }
 
     private getLineChar(line: number[]): Cell[] {
@@ -50,29 +48,29 @@ export class Board {
     }
 
     private static winPos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ]
 
     checkWin(): string {
-        // TODO
-        // Если имеется комбинация из трех одинаковых символов "X" или "0" 
-        //  в линию - возвращает этот символ
-        // Иначе возвращает символ "_"
-        return "_"
+        for (const line of Board.winPos) {
+            const [a, b, c] = this.getLineChar(line);
+            if (a !== "_" && a === b && b === c) {
+                return a;
+            }
+        }
+        return "_";
     }
 
-
     status(): string {
-        // TODO
-        // возвращает либо строку с результатом игры, либо, 
-        //   если игра не закончена, строку "Идет игра".
-        return "Идет игра"
+        const winner = this.checkWin();
+        if (winner !== "_") {
+            return `Победил: ${winner}`;
+        }
+        if (this.isFill()) {
+            return "Ничья!";
+        }
+        return "Идет игра";
     }
 }
